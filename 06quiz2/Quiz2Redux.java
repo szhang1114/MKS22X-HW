26f227ae136bc16c8e39de425de8791a9500c7d4
Steven Zhang <szhang@stuy.edu>
2017-03-14 22:30:55 -0400

    quiz2redux

--- 06quiz2/Quiz2Redux.java
+++ 06quiz2/Quiz2Redux.java
@@ -1,19 +1,32 @@
 import java.util.*;
 public class Quiz2Redux{  
-  /*Returns an ArrayList<String> that contains all subsets of the
-   *characters of String s. Assume s has no duplicate characters.
-   *the characters should appear in the same order that they occur 
-   *in the original string.
-   */
-  public static ArrayList<String> combinations(String s){
-      ArrayList<String>words = new ArrayList<String>();
-      help( words , /*fill this in with more */);
-      Collections.sort(words);
-      return words;
-  }
+    /*Returns an ArrayList<String> that contains all subsets of the
+     *characters of String s. Assume s has no duplicate characters.
+     *the characters should appear in the same order that they occur 
+     *in the original string.
+     */
+    public static ArrayList<String> combinations(String s){
+	ArrayList<String>words = new ArrayList<String>();
+	help( words, s, 0, "");
+	Collections.sort(words);
+	return words;
+    }
   
-  private static void help( ArrayList<String> words, 
-                             /*fill this in with more arguments*/ ){
-   /*METHOD TO BE WRITTEN BY YOU.*/ 
-  }
+    private static void help( ArrayList<String> words, String s, int current, String in){
+	if(current == 0){
+	    words.add(" ");
+	}
+	if(current < s.length()){
+	    words.add(in + s.charAt(current));
+	    help(words, s, current + 1, in + s.charAt(current));
+	    help(words, s, current + 1, in);
+	}
+    }
+    
+     public static void main(String [] args){
+	Quiz2Redux x = new Quiz2Redux();
+	System.out.println(combinations("ab"));
+	System.out.println(combinations("abc"));
+	System.out.println(combinations("abcd"));
+    }
 }
