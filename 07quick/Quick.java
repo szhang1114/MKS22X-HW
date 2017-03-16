1201446603fe5cd19c6098e7e22d78af5fc539d3
Steven Zhang <szhang9@stuy.edu>
2017-03-16 14:48:43 -0400

    quick

--- 07quick/Quick.java
+++ 07quick/Quick.java
@@ -3,21 +3,23 @@
 public class Quick{
 
     public static int part(int[] data, int start, int end){
-	int pivot = (int)(Math.random() * data.length);
+	int range = end - start + 1;
+	int pivotIndex = (int)(Math.random() * range + start);
+	int pivot = data[pivotIndex];
 	
 
-
 	int finalPivotIndex = 0;
-	return finalPivotIndex;
+	return pivot;
     }
 
     public String toString(){
 	return "";
     }
     public static void main(String[] args){
-	int[] ary = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-	System.out.println(part(ary, 1, 0));
-
+	int[] ary = {10, 5, 1, 6, 9, 7, 2, 8, 3, 0, 4};
+	for(int i = 0; i < 10; i ++){
+	    System.out.print(part(ary, 0, 10));
+	    System.out.println("");
+	}
     }
 }
