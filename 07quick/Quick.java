a5cab1e63ed3e3dfaf91a8d5cdda7596715b8c1f
Steven Zhang <szhang@stuy.edu>
2017-03-17 00:08:16 -0400

    quick

--- 07quick/Quick.java
+++ 07quick/Quick.java
@@ -1,5 +1,3 @@
-
-
 public class Quick{
 
     public static int part(int[] data, int start, int end){
@@ -11,9 +9,17 @@ public class Quick{
 	int finalPivotIndex = 0;
 	return pivot;
     }
-
-    public String toString(){
-	return "";
+    private static void swap(int[] data, int oldIndex, int newIndex){
+	int oldInt = data[oldIndex];
+	data[oldIndex] = data[newIndex];
+	data[newIndex] = oldInt;
+    }
+    public String toString(int[] data){
+	String out = "";
+	for(int i = 0; i < data.length(); i ++){
+	    out += data[i] + " ";
+	}
+	return out;
     }
     public static void main(String[] args){
 	int[] ary = {10, 5, 1, 6, 9, 7, 2, 8, 3, 0, 4};
