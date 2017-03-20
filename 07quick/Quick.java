239ed595b89ee9eeea1cc604342800533c6ae085
Steven Zhang <szhang@stuy.edu>
2017-03-20 01:47:22 -0400

    quick

--- 07quick/Quick.java
+++ 07quick/Quick.java
@@ -4,9 +4,21 @@ public class Quick{
 	int range = end - start + 1;
 	int pivotIndex = (int)(Math.random() * range + start);
 	int pivot = data[pivotIndex];
+	System.out.println("pivot = " + pivot);
+	swap(data, pivotIndex, start);
+	int front = start + 1;
 	
-
-	int finalPivotIndex = 0;
+	for(int index = start + 1; index <= end; index ++){
+	    //System.out.println(outString(data));
+	    //System.out.println("front " + front );
+	    if(data[index] < pivot){
+		swap(data, front, index);
+		//System.out.println(outString(data));
+		front ++;
+	    }
+	}
+	int finalPivotIndex = front - 1;
+	swap(data, start, finalPivotIndex);
 	return pivot;
     }
     private static void swap(int[] data, int oldIndex, int newIndex){
@@ -14,18 +26,16 @@ public class Quick{
 	data[oldIndex] = data[newIndex];
 	data[newIndex] = oldInt;
     }
-    public String toString(int[] data){
+    public static String outString(int[] data){
 	String out = "";
-	for(int i = 0; i < data.length(); i ++){
+	for(int i = 0; i < data.length; i ++){
 	    out += data[i] + " ";
 	}
 	return out;
     }
     public static void main(String[] args){
 	int[] ary = {10, 5, 1, 6, 9, 7, 2, 8, 3, 0, 4};
-	for(int i = 0; i < 10; i ++){
-	    System.out.print(part(ary, 0, 10));
-	    System.out.println("");
-	}
+	part(ary, 0, 10);
+	System.out.println(outString(ary));
     }
 }
