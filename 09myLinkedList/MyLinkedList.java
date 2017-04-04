0dc33a9827ed11059140364858b480d0c365eb83
Steven Zhang <szhang9@stuy.edu>
2017-04-04 14:49:19 -0400

    linked

---
+++ 09myLinkedList/MyLinkedList.java
@@ -0,0 +1,67 @@
+public class MyLinkedList{
+    private LNode start;
+    private LNode end;
+    private int size;
+
+    public MyLinkedList(){
+    }
+
+    public void add(int value, int index){
+	if(size == 0){
+	    start = new LNode(value);
+	    end = start;
+	}
+	else{
+	    LNode temp = new LNode(value);
+	    end.next = temp;
+	    end = temp;	    
+	}
+
+	size++;
+    }
+
+    public boolean add(int value){
+	add(value, size);
+	return true;
+    }
+
+    private void addAfter(LNode location, LNode toBeAdded){
+    }
+
+    public int size(){
+	return size;
+    }
+
+
+    public int get(int index){
+	return 
+    }
+
+    public int set(int index, int newValue){
+
+    }
+
+    public String toString(){
+	return "";
+    }
+
+    private class LNode{
+	int value;
+	LNode next;
+	LNode prev;
+
+	public LNode(int value){
+	    this.value = value;
+	}
+	
+	public LNode(int value, LNode next, LNode prev){
+	    this.value = value;
+	    this.next = next;
+	    this.previous = prev;
+	}
+	public String toString(){
+	    return value + "";
+	}
+	
+    }
+}
