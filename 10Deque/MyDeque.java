c58ed863fb9664d23f57e8cb777fd14ce6d13c36
Steven Zhang <szhang@stuy.edu>
2017-04-25 00:26:07 -0400

    new

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,25 @@
+public class MyDeque{
+
+    public void addFirst(String s){
+	
+    }
+
+    public void addLast(String s){
+	
+    }
+
+    public String removeFirst(){
+	
+    }
+    public String removeLast(){
+	
+    }
+
+    public String getFirst(){
+	
+    }
+
+    public String getLast(){
+
+    }
+}
