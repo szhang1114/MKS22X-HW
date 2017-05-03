5a7ef605ea516db3ec2ba7ba9b594d2e0e6d3d0f
Steven Zhang <szhang9@stuy.edu>
2017-05-03 14:49:51 -0400

    myheap

---
+++ 13MyHeap/MyHeap.java
@@ -0,0 +1,37 @@
+public class MyHeap{
+
+    public MyHeap(){
+	maxheap;
+    }
+
+    public MyHeap(boolean maxmin){
+	if(maxmin){
+	    maxheap;
+	}
+	else minheap;
+    }
+
+    public void add(String s){
+    }
+
+    public String remove(){
+    }
+
+    public String peek(){
+	return index1;
+    }
+
+    private void pushUp(){
+    }
+
+    private void pushDown(){
+    }
+    
+    private int Rchild(int n){
+    }
+
+    private int Lchild(int n){
+    }
+
+    private int parent(int n){
+    }
