5acb4bca202cce1aa7a9cd12655b2cee88e7e973
Steven Zhang <szhang@stuy.edu>
2017-05-14 23:09:05 -0400

    deq

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,5 +1,13 @@
 public class MyDeque{
-
+    private String[] deque;
+    private int front, back, size;
+    
+    public MyDeque(){
+	size = 1;
+	deque = new String[size];
+	front = 0;
+	back = 0;	
+    }
     public void addFirst(String s){
 	
     }
@@ -9,17 +17,26 @@ public class MyDeque{
     }
 
     public String removeFirst(){
-	
+	return "";
     }
     public String removeLast(){
-	
+	return "";
     }
 
     public String getFirst(){
-	
+	return "";
     }
 
     public String getLast(){
+	return "";
+    }
 
+    public void resize(){
+	size *= 2;
+	String[] temp = new String[size];
+	for(int i = front; i < deque.length;  i ++){
+	    //if(i 
+	    temp[i] = deque[i];
+	}
     }
 }
