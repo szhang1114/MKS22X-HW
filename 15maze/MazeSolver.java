635f62dcac9369039a34a9ed2f7be907252975e1
Steven Zhang <szhang@stuy.edu>
2017-05-16 23:38:32 -0400

    maze

---
+++ 15maze/MazeSolver.java
@@ -0,0 +1,24 @@
+public class MazeSolver{
+
+    private Maze board;
+
+
+    public MazeSolver(String filename){
+	this(filename, false);
+    }
+
+    public MazeSolver(String filename, boolean animate){
+
+    }
+    public void solve(){
+
+    }
+
+    public void solve(int style){
+
+    }
+
+    public String toString(){
+	return "";
+    }
+}
